7348baf1c822f1a58136ed7ce10f862e4df18110
folarmi <fola4ril@gmail.com>
2020-03-07 18:58:15 -0800

    added expense title and amount

--- app.js
+++ app.js
@@ -4,9 +4,15 @@ const myBudget = document.getElementById('my-budget');
 const expenseName = document.getElementById('expense-name');
 const expenseAmount = document.getElementById('expense-amount');
 const listExpenses = document.getElementsByClassName('list-expenses');
+const expenseButton = document.getElementById('expense-button');
+const listItems = document.getElementById('list-items');
+const li = document.getElementById('add');
+const addSpan = document.getElementById('add-span');
 
 //Event Listeners
-myBudget.addEventListener("keydown",getAmount)
+myBudget.addEventListener("keydown",getAmount);
+expenseButton.addEventListener('click', addExpenseItem);
+
 
 //Functions
 function getAmount(event) {
@@ -15,4 +21,20 @@ function getAmount(event) {
         amtBudget.textContent = "Budget:" + "$" + myBudget.value;
         myBudget.value = '';
     }
+}
+
+
+function addExpenseItem(){ 
+    const newLi = document.createElement('li');
+    const newSpan = document.createElement('span');
+    newSpan.classList.add('amount')
+    
+    const newExpenseName = expenseName.value;
+    const newExpenseAmount = expenseAmount.value;
+
+    newSpan.append(newExpenseAmount);
+    listItems.appendChild(newLi).append(newExpenseName,newSpan);
+
+    expenseAmount.value = '';
+    expenseName.value = '';
 }
