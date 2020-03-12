94df49e78849ae696d84e4f070168feada5b3191
folarmi <fola4ril@gmail.com>
2020-03-11 20:50:33 -0700

    added validation

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const listItems = document.getElementById('list-items');
 const li = document.getElementById('add');
 const addSpan = document.getElementById('add-span');
 var amtLeft = document.getElementById('amt-left');
+const myForm = document.forms.my
+
 
 
 //Event Listeners
@@ -16,20 +18,31 @@ myBudget.addEventListener("keydown",getAmount);
 expenseButton.addEventListener('click', addExpenseItem);
 
 
+
 //Functions
 function getAmount(event) {
     if (event.keyCode === 13){
         event.preventDefault();
+        if (myBudget.value === 0 || myBudget.value === ''){
+            alert("Please enter a valid amount")
+        }
         amtBudget.textContent = "Budget:" + "$" + myBudget.value;
         amtLeft.textContent = "Left:" + "$" + myBudget.value;
         amtLeft.value = myBudget.value;
         myBudget.textContent = '';
-       
     }
 }
 
 
-function addExpenseItem(){ 
+function addExpenseItem(event){ 
+  event.preventDefault()
+    if(myForm.elements.expense.value === "" && myForm.elements.amount.value === ""){
+        expenseButton.setAttribute('disabled', true);
+        alert('Fill out all fields')
+    } else {
+        expenseButton.removeAttribute('disabled')
+    }
+
     const newLi = document.createElement('li');
     const newSpan = document.createElement('span');
     newSpan.classList.add('amount')
@@ -39,14 +52,4 @@ function addExpenseItem(){
 
     newSpan.append(newExpenseAmount);
     listItems.appendChild(newLi).append(newExpenseName,newSpan);
-    
-    trackBudget()
-    expenseAmount.value = '';
-    expenseName.value = '';
 }
-
-function trackBudget() {
-    amtLeft -= expenseAmount.value;
-    console.log(amtLeft)
-}
-
